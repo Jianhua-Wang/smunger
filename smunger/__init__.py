57cd892409fcb1aa4f0204809b206f5f1d57d11e
Jianhua Wang <jianhua.mert@gmail.com>
2023-02-24 11:03:01 +0800

    Bump version: 0.0.6 → 0.0.7

--- smunger/__init__.py
+++ smunger/__init__.py
@@ -28,7 +28,7 @@ from .annotate import annotate_rsid
 
 __author__ = """Jianhua Wang"""
 __email__ = 'jianhua.mert@gmail.com'
-__version__ = '0.0.6'
+__version__ = '0.0.7'
 
 # Set up logging
 logging.basicConfig(
